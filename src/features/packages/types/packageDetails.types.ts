ae73a4283672abbbe0a6347de7f5ae84ab72cb36
koenamittalmarch18-creator <koenamittalmarch18@gmail.com>
2026-08-23 00:26:13 +0530

    Cleanup duplicate frontend and remove node_modules

--- src/features/packages/types/packageDetails.types.ts
+++
@@ -1,8 +0,0 @@
-export interface PackageDetails {
-    packageId: string;
-    overview: string;
-    inclusions: string[];
-    exclusions: string[];
-    highlights: string[];
-    gallery: string[];
-  }
